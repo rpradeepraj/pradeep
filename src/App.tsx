77b8d492d203155cb0a402ff9fa36f7212f56747
rpradeepraj <rpredeepraj@gmail.com>
2025-12-29 12:32:49 +0530

    remove apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Layout from './components/layout/Layout';
 import Hero from './components/sections/Hero';
 import About from './components/sections/About';
-import Apps from './components/sections/Apps';
+// import Apps from './components/sections/Apps';
 import Experience from './components/sections/Experience';
 import Skills from './components/sections/Skills';
 import GitHubOverview from './components/sections/GitHubOverview';
